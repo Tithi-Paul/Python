e9e7c4afe9d0429dc522dcd3f519ea906c771417
Tithi Paul <tithi.cse3.bu@gmail.com>
2022-03-02 11:31:06 +0600

    Basics of turtle

---
+++ Turtle/equilateral_triangle with turtle.py
@@ -0,0 +1,12 @@
+import turtle
+
+#turtle.shape("turtle")
+turtle.speed(2) #0 means highest speed. 1 means lowest speed. Gradually increase till 10.
+
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.exitonclick()
---
+++ Turtle/square in turtle.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.shape("turtle")
+turtle.speed(2) #0 means highest speed. 1 means lowest speed. Gradually increase till 10.
+
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.exitonclick()
